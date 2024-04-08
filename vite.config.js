07db1b8e459db018182fee347c3bf56c580333c6
RyanFields <rienfeels14@gmail.com>
2024-04-08 11:47:54 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import css from "rollup-plugin-css-only";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), css({ include: "**/*.css" })],
+  plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
